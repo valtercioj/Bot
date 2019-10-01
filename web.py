b84319317f58f43db871741da3b4cde0fd707deb
valtercioj <53585971+valtercioj@users.noreply.github.com>
2019-09-30 23:56:14 -0300

    Update web.py

--- web.py
+++ web.py
@@ -9,25 +9,10 @@ def site(): # funcao para abrir sites
         opcao = str(input('Qual site deseja abrir? = ')) # escolha do site
 
 ##################################################################################
-# links de sites
-        if opcao == 'moodle imd':
-            os.startfile("https://moodle.imd.ufrn.br/login/index.php")
-        if opcao == 'suap':
-            os.startfile("https://suap.ifrn.edu.br")
-        if opcao == 'sigaa':
-            os.startfile("https://sigaa.ufrn.br/sigaa")
-        if opcao == 'github':
-            os.startfile("https://github.com/valtercioj")
-        if opcao == 'facebook':
-            os.startfile("https://pt-br.facebook.com")
-        if opcao == 'udemy':
-            os.startfile("https://www.udemy.com/")
-        if opcao == 'youtube':
-            os.startfile("https://www.youtube.com/")
-        if opcao == 'google':
-            os.startfile("https://www.google.com/")
-        if opcao == 'materiais imd':
-            os.startfile("https://materiais.imd.ufrn.br")
+    
+        p = str(input('onde deseja buscar, se for link direto so aperte enter: '))
+        for url in search(opcao+p, stop=2):
+	        os.startfile(url)
 ######################################################################################       
         print()
         print('+=+'*20)
@@ -37,25 +22,10 @@ def site(): # funcao para abrir sites
     elif plataforma == 'linux': # para lixus
         opcao = str(input('Qual site deseja abrir? = ')) # entrada do site
 ##################################################################################
-# links de sites
-        if opcao == 'moodle imd':
-            s.Popen(["xdg-open","https://moodle.imd.ufrn.br/login/index.php"])
-        if opcao == 'suap':
-            s.Popen(["xdg-open","https://suap.ifrn.edu.br"])
-        if opcao == 'sigaa':
-            s.Popen(["xdg-open","https://sigaa.ufrn.br/sigaa"])
-        if opcao == 'github':
-            s.Popen(["xdg-open","https://github.com/valtercioj"])
-        if opcao == 'facebook':
-            s.Popen(["xdg-open","https://pt-br.facebook.com"])
-        if opcao == 'udemy':
-            s.Popen(["xdg-open","https://www.udemy.com/"])
-        if opcao == 'youtube':
-            s.Popen(["xdg-open","https://www.youtube.com/"])
-        if opcao == 'google':
-            s.Popen(["xdg-open","https://www.google.com/"])        
-        if opcao == 'materiais imd':
-            s.Popen(["xdg-open","https://materiais.imd.ufrn.br"])
+      
+        p = str(input('onde deseja buscar, se for link direto so aperte enter: '))
+        for url in search(opcao+p, stop=2):
+	        os.startfile(url)
 ######################################################################################        
         print()
         print('+=+'*20)
