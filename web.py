32194475d8fd4075475578277eefd6cb79a3be7a
valtercioj <53585971+valtercioj@users.noreply.github.com>
2019-10-07 16:59:49 -0300

    Update web.py

--- web.py
+++ web.py
@@ -26,7 +26,7 @@ def site(): # funcao para abrir sites
       
         p = str(input('onde deseja buscar, se for link direto so aperte enter: '))
         for url in search(opcao+p, stop=1):
-	        s.Popen(['xd-open'+url])
+	        s.Popen(['xdg-open',url])
 ######################################################################################        
         print()
         print('+=+'*20)
