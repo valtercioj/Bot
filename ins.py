ab02d8555f6facaeac4e3bb5d9b216b55676a13c
valtercioj <53585971+valtercioj@users.noreply.github.com>
2019-10-21 10:50:06 -0300

    Update ins.py

--- ins.py
+++ ins.py
@@ -1,15 +1,15 @@
 import os
 import sys
 
-
-plat = sys.platform
-if plat == 'win32':
-    os.system('pip install google')
-    os.system('pip install wikipedia')
-    os.system('pip install chatterbot')
-    os.system('pip install youtube-dl')
-elif plat == 'linux':
-    os.system('pip3 install google')
-    os.system('pip3 install wikipedia')
-    os.system('pip3 install chatterbot')
-    os.system('pip3 install youtube-dl')
+def ins():
+    plat = sys.platform
+    if plat == 'win32':
+        os.system('pip install google')
+        os.system('pip install wikipedia')
+        os.system('pip install chatterbot')
+        os.system('pip install youtube-dl')
+    elif plat == 'linux':
+        os.system('pip3 install google')
+        os.system('pip3 install wikipedia')
+        os.system('pip3 install chatterbot')
+        os.system('pip3 install youtube-dl')
