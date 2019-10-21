57ee18f583e765d07c199f8227864647c90b3e50
valtercioj <53585971+valtercioj@users.noreply.github.com>
2019-10-21 15:08:10 -0300

    Update ins.py

--- ins.py
+++ ins.py
@@ -7,9 +7,9 @@ def ins():
         os.system('pip install google')
         os.system('pip install wikipedia')
         os.system('pip install chatterbot')
-        os.system('pip install youtube-dl')
+        
     elif plat == 'linux':
         os.system('pip3 install google')
         os.system('pip3 install wikipedia')
         os.system('pip3 install chatterbot')
-        os.system('pip3 install youtube-dl')
+        
