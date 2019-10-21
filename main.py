88c566e62f6fe102efc91f84b1df04fda5231386
valtercioj <53585971+valtercioj@users.noreply.github.com>
2019-10-21 10:43:05 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+from chat import conv
+from google import google
+from web import site
+from wikipedia import wiki
+from trend import trend
+from cpf import cpf
+from ins import ins
+
+ins()
+print('''
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+     ola Valtercio bem vindo de volta. Estou a sua desposição.
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+''')
+
+
+perg = str(input('Voce: ')) # entrada com o nome jarvis
+print()
+
+jarvis = ''
+
+if perg == 'não quero fazer nada': # comando para sair do Bot
+    print()# para da espaços entre as linhas
+    print('Ok senhor, até a proxima...')
+
+if perg == 'jarvis': # comando para iniciar o Bot
+    jarvis = str(input('ola senhor o que deseja? ')) # escolha das funções do Bot.
+
+print() # para da espaços entre as linhas
+
+if jarvis == 'faca uma busca' or jarvis == 'faça uma busca': # Bot faz uma busca no google ou youtube        
+	google()
+if jarvis == 'abra um site' or jarvis == 'quero abrir um site':  # Bot abrirá um site
+        site() # funcao para abrir sites
+if jarvis == 'quero conversar': # para conversar com o Bot
+        conv() # funcao do chat
+if jarvis == 'faca uma busca na wikipedia' or jarvis == 'faça uma busca na wikipedia': # Bot pesquisa na wikipedia
+	wiki() # funcao da wikipedia
+if jarvis == 'faca uma busca na trend':
+	trend()
+if jarvis == 'faca uma busca por cpf':
+	cpf()    
