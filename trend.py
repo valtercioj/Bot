127617298c908b88ad2bcfcecee2ac5fba27faa8
valtercioj <53585971+valtercioj@users.noreply.github.com>
2019-11-09 09:23:28 -0300

    Update trend.py

--- trend.py
+++ trend.py
@@ -1,6 +1,6 @@
 import webbrowser
 def trend():
     pg = str(input('o que deseja perquisar: '))
-    trend = ('https://trends.google.com.br/trends/explore?q=') # busca no youtube
+    trend = ('https://trends.google.com.br/trends/explore?q=') # busca no google trend
     pesq = (trend + pg)
     webbrowser.open(pesq)
